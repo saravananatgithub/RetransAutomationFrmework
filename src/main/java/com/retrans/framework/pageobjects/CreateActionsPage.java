2d6db1abc8a1f010d65be86912041b51699228eb
saravananatgithub <38750533+saravananatgithub@users.noreply.github.com>
2022-10-04 16:44:47 +0530

    Update CreateActionsPage.java

--- src/main/java/com/retrans/framework/pageobjects/CreateActionsPage.java
+++ src/main/java/com/retrans/framework/pageobjects/CreateActionsPage.java
@@ -1,16 +1,34 @@
 package com.retrans.framework.pageobjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-public class CreateActionsPage {
+public class CreateNotesPage extends BasePage {
 
-	public CreateActionsPage(WebDriver driver) {
-		// TODO Auto-generated constructor stub
+	@FindBy(xpath = "//input[@id='litPanel:createUserNotesForm:title']")
+	private WebElement inputNotesTitle;
+
+	@FindBy(xpath = "//input[@id='litPanel:createUserNotesForm:notes']")
+	private WebElement inputDescription;
+
+	@FindBy(xpath = "//button[@id='litPanel:createUserNotesForm:saveNote']//span[@class='ui-button-text ui-c'][normalize-space()='Save']")
+	private WebElement saveNotesButton;
+
+	public CreateNotesPage(WebDriver driver) {
+		super(driver);
 	}
 
-	public void createAction(String title, String description) {
-		// TODO Auto-generated method stub
-		
+	public void addNotes(String title, String description) {
+
+		driver.findElement(By.xpath("//input[@id='litPanel:createUserNotesForm:title']")).sendKeys(title);
+
+		driver.findElement(By.xpath("//textarea[@id='litPanel:createUserNotesForm:notes']")).sendKeys(description);
+
+		driver.findElement(By.xpath(
+				"//button[@id='litPanel:createUserNotesForm:saveNote']//span[@class='ui-button-text ui-c'][normalize-space()='Save']"))
+				.click();
 	}
 
 }
