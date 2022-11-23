cdf56f9b6219b9e2aa64ffd829e31e01d86b94c1
saravananatgithub <38750533+saravananatgithub@users.noreply.github.com>
2022-11-23 19:44:44 +0530

    filter tests separated

--- src/main/java/com/retrans/test/RetransTest.java
+++ src/main/java/com/retrans/test/RetransTest.java
@@ -48,13 +48,13 @@ public class RetransTest extends TestBase {
 	public void editLiteratureItemAddNotes() {
 
 		listLiteItemsPage.navigateToListLitItemsPage();
-		EditLiteraturesPage editLitPage = listLiteItemsPage.editLiterature("2022_W46_567");
+		EditLiteraturesPage editLitPage = listLiteItemsPage.editLiterature("2022_W47_5");
 		threadwait(3000);
 		editLitPage.createNotes("Nodes Title Nov 22 2022", "Notes Description Nov 22 2022 Description", "");
 		threadwait(3000);
 	}
 
-	@Test
+	// @Test
 	public void editLiteratureItemAddNotesWithAttachment() {
 
 		listLiteItemsPage.navigateToListLitItemsPage();
@@ -99,13 +99,52 @@ public class RetransTest extends TestBase {
 		threadwait(5000);
 	}
 
-	// @Test
-	public void filterLiteratures() {
+	@Test
+	public void filterLiteraturesByActivity() {
 		listLiteItemsPage.navigateToListLitItemsPage();
 		listLiteItemsPage.filterByActivity("Triage");
 		threadwait(3000);
 		listLiteItemsPage.clearFilter();
 		threadwait(5000);
+	}
+
+	@Test
+	public void filterLiteraturesByClassification() {
+		listLiteItemsPage.navigateToListLitItemsPage();
+
+		listLiteItemsPage.filterByClassification("ICSR");
+		threadwait(3000);
+		listLiteItemsPage.clearFilter();
+		threadwait(5000);
+	}
+
+	@Test
+	public void filterLiteraturesByBusinessUnit() {
+		listLiteItemsPage.navigateToListLitItemsPage();
+
+		listLiteItemsPage.filterByBusinessUnit("Testing-BU");
+		threadwait(3000);
+		listLiteItemsPage.clearFilter();
+		threadwait(6000);
+	}
+
+	@Test
+	public void filterLiteraturesByClaims() {
+		listLiteItemsPage.navigateToListLitItemsPage();
+
+		listLiteItemsPage.filterByClaims("Claimed by me");
+		threadwait(3000);
+		listLiteItemsPage.clearFilter();
+		threadwait(5000);
+	}
+
+	// @Test
+	public void filterLiteratures() {
+		listLiteItemsPage.navigateToListLitItemsPage();
+//		listLiteItemsPage.filterByActivity("Triage");
+//		threadwait(3000);
+//		listLiteItemsPage.clearFilter();
+//		threadwait(5000);
 
 		listLiteItemsPage.filterByClassification("Duplicate");
 		threadwait(3000);
